60d79c6e23ede77e5ad62b4824f62ee31201de60
Eric Ingratta <eingratt@uwo.ca>
2018-11-25 03:23:47 +0000

    Added Services to handle recipes and shopping-list

--- lab5v3/src/app/recipes/recipe-list/recipe-list.component.ts
+++ lab5v3/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,15 +8,14 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  
-  recipes: Recipe[]=[
-    new Recipe('A test Recipe', 'This is a test','https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg'),
-    new Recipe('A test Recipe', 'This is a test','https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg')
-    ];
 
-  constructor() { }
+  recipes: Recipe[]=[];
+
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
-
+  
+ 
 }
---
+++ lab5v3/src/app/recipes/recipe.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  
+  recipeSelected = new EventEmitter<Recipe>();
+  
+  private recipes: Recipe[]=[
+    new Recipe('A test Recipe', 
+    'This is a test',
+    'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg',
+    [
+      new Ingredient('Meat',1),
+      new Ingredient('French Fries',2)
+      ]),
+    new Recipe('Bobs Big Burger', 'This is a test',
+    'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg',
+    [
+      new Ingredient('Buns',2),
+      new Ingredient('Patty',3) 
+      ])
+  ];
+  
+  constructor(private slService: ShoppingListService) { }
+  
+  getRecipes(){
+    return this.recipes.slice();
+    
+  }
+  
+  addIngredientToShoppingList(ingredients: Ingredient[]){
+    this.slService.addIngredients(ingredients);
+    
+  }
+ 
+}
